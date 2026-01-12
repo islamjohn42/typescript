7d518bc4e65be2a90bac97c720b12c097c544743
islamjohn42 <islomjohn.uz@gmail.com>
2026-01-12 14:35:19 +0500

    init: Add typescript folder

---
+++ app.js
@@ -0,0 +1,17 @@
+"use strict";
+const userName = "Islam";
+console.log(userName);
+let a = 5;
+if (a === 5) {
+    console.log("true");
+}
+else
+    console.log("Not worked");
+const numbers = [1, 2, 3];
+const anotherNumbers = [1, 2, 3];
+const userInfo = {
+    fName: "Ali",
+    lName: "Smith",
+    age: 31,
+};
+console.log(userInfo.age);
---
+++ app.ts
@@ -0,0 +1,17 @@
+const userName: string = "Islam";
+console.log(userName);
+
+let a: number = 5;
+if (a === 5) {
+  console.log("true");
+} else console.log("Not worked");
+
+const numbers: number[] = [1, 2, 3];
+const anotherNumbers: Array<number> = [1, 2, 3];
+
+const userInfo: { fName: string; lName: string; age: number } = {
+  fName: "Ali",
+  lName: "Smith",
+  age: 31,
+};
+console.log(userInfo.age);
