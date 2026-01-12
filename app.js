92cecd9461fa8895a12ecd6176c343538ad0803a
islamjohn42 <islomjohn.uz@gmail.com>
2026-01-12 15:06:11 +0500

    feat: Add Function overloading

--- app.js
+++ app.js
@@ -15,3 +15,29 @@ const userInfo = {
     age: 31,
 };
 console.log(userInfo.age);
+// ---------------------Function---------------------
+const add = (a, b) => {
+    return a + b;
+};
+console.log(add(5, 6));
+const getGreeting = (fName, lName) => {
+    return `Hello ${fName + lName}`;
+};
+console.log(getGreeting("Ali", "Smith"));
+// ---------------------Function overloading---------------------
+// Function signature
+// Overload signature
+// Implementation signature
+function sayHelloFunctionSignature(name) {
+    return `Hello there, ${name}!`;
+}
+// Implementation signature
+function sayHello(name) {
+    if (typeof name === "string") {
+        return `Hello there, ${name}!`;
+    }
+    else if (Array.isArray(name)) {
+        return name.map((name) => `Hello, ${name}!`);
+    }
+    throw new Error("Something went wrong");
+}
--- app.ts
+++ app.ts
@@ -15,3 +15,37 @@ const userInfo: { fName: string; lName: string; age: number } = {
   age: 31,
 };
 console.log(userInfo.age);
+
+// ---------------------Function---------------------
+const add = (a: number, b: number) => {
+  return a + b;
+};
+console.log(add(5, 6));
+
+const getGreeting = (fName: string, lName: string): string => {
+  return `Hello ${fName + lName}`;
+};
+console.log(getGreeting("Ali", "Smith"));
+
+// ---------------------Function overloading---------------------
+// Function signature
+// Overload signature
+// Implementation signature
+function sayHelloFunctionSignature(name: string): string {
+  return `Hello there, ${name}!`;
+}
+
+// Overload signature
+function sayHello(name: string): string;
+function sayHello(name: string[]): string[];
+
+// Implementation signature
+function sayHello(name: unknown): unknown {
+  if (typeof name === "string") {
+    return `Hello there, ${name}!`;
+  } else if (Array.isArray(name)) {
+    return name.map((name) => `Hello, ${name}!`);
+  }
+
+  throw new Error("Something went wrong");
+}
